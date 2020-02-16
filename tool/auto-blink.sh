db0a6fa0ebb2a70ea15e789c4eefbc6aaa05496d
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-16 23:39:39 +0900

    Change: tool/auto-blink> order of auto blink

--- tool/auto-blink.sh
+++ tool/auto-blink.sh
@@ -2,9 +2,9 @@ function continueWink() {
   trap 'exit; trap 30' 30
   local -i interval=$1
 
-  tmux send -t {left-of} 'sd'
-  sleep 0.3
   tmux send -t {left-of} 'we'
+  sleep 0.3
+  tmux send -t {left-of} 'sd'
   sleep $interval
 
   continueWink $interval
